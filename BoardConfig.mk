efc040c702ccef39809707900590a3e3581fad24
leefshitz2 <leefshitz2@gmail.com>
2025-06-25 16:38:14 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,7 @@ TW_HAS_NO_RECOVERY_PARTITION := true
 #TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/sprd_backlight/backlight/sprd_backlight/brightness"
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TW_EXTRA_LANGUAGES := true
+TW_EXTRA_LANGUAGES := false
 TW_DEFAULT_LANGUAGE := ru
 TW_THEME := portrait_hdpi
 TW_INPUT_BLACKLIST := "hbtp_vm"
@@ -158,7 +158,8 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_USB_STORAGE := true
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TW_INCLUDE_LPTOOLS := true
-TW_DEVICE_VERSION := DoogeeX96 by depesh1977
+TW_DEVICE_VERSION := PBRP Doogee X96 by leefshitz
+TW_OEM_BUILD := true
 
 # Decryption
 TW_INCLUDE_CRYPTO := false
